8f4a15361dbcd140c28709637d2d2959c138fca4
Spencer Gouw <swyverng55@g.ucla.edu>
2020-08-18 20:29:40 -0700

    Not working...

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 #! python3
 
+import errno
 import os, sys
 import argparse
 import time
@@ -61,27 +62,34 @@ into it; no need for work.  Typing in the filenames is the harder part.
 
 if sys.platform == "win32":
     print("Windows OS detected.")
-    WRITE_NAME = "\\\\.\\pipe\\ToSrvPipe"
-    READ_NAME = "\\\\.\\pipe\\FromSrvPipe"
-    EOL = "\r\n\0"
+    WRITE_NAME = '\\\\.\\pipe\\ToSrvPipe'
+    READ_NAME = '\\\\.\\pipe\\FromSrvPipe'
+    EOL = '\r\n\0'
 else:
     print("Unix-like OS detected.")
-    WRITE_NAME = "/tmp/audacity_script_pipe.to." + str(os.getuid())
-    READ_NAME = "/tmp/audacity_script_pipe.from." + str(os.getuid())
-    EOL = "\n"
+    WRITE_NAME = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
+    READ_NAME = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
+    EOL = '\n'
 
 
 class AudacityInstance:
 
-    reader_pipe_broken = threading.event()
-    reply_ready = threading.event()
+    # These are global to the class, remember - a form of shared state, even between threads.
+    # This is a form of shared mutable state between threads.  The threads communicate
+    # using these variables.  The instance variable in question for the second is self._reply.
+    # The write and read threads communicate with the threading event object, and when the event object says
+    # "ok, it's set/unset", then they know to access the variable.
+    # They can be set and unset from anywhere.
+    reader_pipe_broken = threading.Event()
+    # Unset when sending a command, set when receiving a response.
+    reply_ready = threading.Event()
 
     # In Python every object has a __dict__ attribute, which contains their symbol table.
     # It's because everything in Python is an object.
 
     def __init__(self):
         self._write_pipe = None
-        self._reply = ''
+        self._reply = ""
         self._write_thread_start()
         self._read_thread_start()
 
@@ -89,39 +97,99 @@ class AudacityInstance:
         # We open the pipe in a new thread, so that if Audacity isn't running, the program does not freeze/shut down.
         # To use Thread() and create a thread object, we pass a callable object to the constructor.
         # write_thread is now a thread_object.
+        # As long as this instance lives, this thread will live, up until the
+        # other non-daemon threads close (and the program exits).
         write_thread = threading.Thread(target=self._write_pipe_open)
         # Let's make it a daemon thread.  If there are no daemon threads left, the program doesn't necessarily exit.
         # If all the non-daemon threads are gone, however, the program will exit.
+        # Deamon threads are useful for background threads that stay alive only because they serve
+        # other more purposeful function in the program; once the program's purpose is served, we don't
+        # have to expend the effort to go and manually close them.  This is great for a pipe client, though we don't
+        # need them to be daemon necessarily.
         write_thread.daemon = True
         # Now we call the function we passed to the thread.
         write_thread.start()
+        # We let some time for the connection to be made (from the thread above).
+        time.sleep(0.2)
+        if not self._write_pipe:
+            sys.exit("Write pipe could not be opened.")
 
     def _write_pipe_open(self):
-        self._write_pipe = open(WRITE_NAME)
+        self._write_pipe = open(WRITE_NAME, 'w')
 
-    def send_command(self, command):
-        """Send a single command to the file handle."""
-        print("Send >>>")
-        print(command)
-        self.write_pipe.write(command + self.eol)
-        self.write_pipe.flush()
+    def _read_thread_start(self):
+        read_thread = threading.Thread(target=self._reader)
+        read_thread.daemon = True
+        read_thread.start()
 
-    def receive_response(self):
-        """Receive a response from Audacity."""
-        print("Receive <<<")
-        response = ""
-        line = ""
+    def _reader(self):
+        """Read FIFO in worker thread."""
+        read_pipe = open(READ_NAME, 'r')
+        message = ""
+        pipe_ok = true
+        while pipe_ok:
+            line = read_pipe.readline()
+            while pipe_ok and line != "\n":
+                message += line
+                # No data in pipe indicates Audacity has crashed.
+                if line == "":
+                    AudacityInstance.reader_pipe_broken = True
+                    pipe_ok = False
+            # When we receive a \n, we mark to the class that the reply has been set.
+            self.reply = message
+            AudacityInstance.reply_ready.set()
+            message = ""
+        read_pipe.close()
 
-        # Response terminates on \n alone.
-        while line != "\n":
-            response += line
-            line = self.read_pipe.readline()
-        print(response)
+    def write(self, command):
+        print("Sending command:", command)
+        self._write_pipe.write(command + EOL)
+        # Check that the read pipe  is still alive
+        # We don't want to send commands if we can't receeive responds.
+        if AudacityInstance.reader_pipe_broken.isSet():
+            sys.exit(
+                "The read pipe received an error.  Audacity has probably crashed or ran into an error."
+            )
+        try:
+            self._write_pipe.flush()
+            self.reply = ""
+            AudacityInstance.reply_ready.clear()
+        except IOError as e:
+            if e.errno == errno.EPIPE:
+                sys.exit("The write pipe received an error.")
+            # If the error isn't regular, we need to raise the error to send it to the call stack.
+            else:
+                raise
+
+    def read(self):
+        # If the reply flag is not ready, then there can be no reading done.
+        if not AudacityInstance.reply_ready.isSet():
+            return ""
+        return self.reply
+
+    # def send_command(self, command):
+    #     """Send a single command to the file handle."""
+    #     print("Send >>>")
+    #     print(command)
+    #     self.write_pipe.write(command + self.eol)
+    #     self.write_pipe.flush()
+    #
+    # def receive_response(self):
+    #     """Receive a response from Audacity."""
+    #     print("Receive <<<")
+    #     response = ""
+    #     line = ""
+    #
+    #     # Response terminates on \n alone.
+    #     while line != "\n":
+    #         response += line
+    #         line = self.read_pipe.readline()
+    #     print(response)
 
     def do_command(self, command):
         """Perform a single command and print the response."""
-        self.send_command(command)
-        self.receive_response()
+        self.write(command)
+        print(self.read())
 
 
 def start_audacity():
@@ -153,7 +221,7 @@ def connect():
     TOFILE = open(WRITE_NAME, "w")
     FROMFILE = open(READ_NAME, "rt")
 
-    instance = AudacityInstance(WRITE_NAME, READ_NAME, EOL, TOFILE, FROMFILE)
+    instance = AudacityInstance()
     return instance
 
 
@@ -203,7 +271,6 @@ def main():
 
     start_audacity()
     instance = connect()
-    wait_for_startup()
     instance.do_command(import2(lof_filepath))
 
 
@@ -216,4 +283,3 @@ if __name__ == "__main__":
         print(
             "One of your input files was not found! It may have been an erroneous filename, or an improper path."
         )
-
